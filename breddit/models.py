1d4ec21626d6812388e083f0f4f41b5d2b49bb03
Danny Yu <tiramisu@Dannys-MBP.home>
2022-11-21 18:32:24 -0500

    add UserManager and superuser

--- breddit/models.py
+++ breddit/models.py
@@ -1,11 +1,36 @@
 from django.db import models
 from datetime import datetime
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
-class User(AbstractUser):
+
+class UserManager(BaseUserManager):
+  def create_user(self, username, password=None, **kwargs):
+    if username is None:
+      raise TypeError('Need a Username')
+    user = self.model(username=username)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+  
+  def create_superuser(self, username, password):
+    if password is None:
+      raise TypeError("Need Password")
+    if username is None:
+      raise TypeError("Need Username")
+    user = self.create_user(username, password)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
+    return user
+    
+class User(AbstractUser, PermissionsMixin):
   created_at = models.DateTimeField(default=datetime.now)
   avatar = models.ImageField(upload_to='avatars', default='default-avatar.png')
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  
+  objects = UserManager()
   
   def __str__(self):
     return self.username
