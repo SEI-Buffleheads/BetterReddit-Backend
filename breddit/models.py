2ba39b465b1f7bcb821af15cda0b5e23590b4b59
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-11-22 11:28:31 -0500

    full crud for post and commenet with auth

--- breddit/models.py
+++ breddit/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
   title = models.CharField(max_length=256)
   body = models.CharField(max_length=256)
   created_at = models.DateTimeField(default=datetime.now)
-  votes = models.IntegerField(blank=True, null=True)
+  votes = models.IntegerField(default=0, null=True)
   
   def __str__(self):
     return self.title
@@ -48,9 +48,9 @@ class Post(models.Model):
 class Comment(models.Model):
   owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE, null=True)
   body = models.CharField(max_length=256)
-  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
   created_at = models.DateTimeField(default=datetime.now)
-  votes = models.IntegerField(blank=True, null=True)
+  votes = models.IntegerField(default=0, null=True)
   
   def __str__(self):
     return self.owner
--- breddit/routers.py
+++ breddit/routers.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from .views import UserViewSet, LoginViewSet, RegistrationViewSet, RefreshViewSet
+from .views import UserViewSet, LoginViewSet, RegistrationViewSet, RefreshViewSet, PostViewSet, CommentViewSet
 
 routes = SimpleRouter()
 
@@ -10,6 +10,10 @@ routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 # USER
 routes.register(r'user', UserViewSet, basename='user')
+
+# Post and Comments
+routes.register(r'posts', PostViewSet)
+routes.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     *routes.urls,
