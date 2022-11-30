e4e289127f6a4d83e03ce04e630e93ded2d2d4ce
Danny Yu <tiramisu@Dannys-MBP.home>
2022-11-30 00:34:37 -0500

    update banner and avatar

--- breddit/views.py
+++ breddit/views.py
@@ -178,10 +178,6 @@ class UpdateBannerView(generics.UpdateAPIView):
     model = User
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self, queryset=None):
-        obj = self.request.user
-        return obj
-
     def update(self, request, *args, **kwargs):
         self.object = self.get_object()
         serializer = self.get_serializer(data=request.data)
