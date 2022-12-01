b9b289f49c011bd5a4243d88e020a454095fc1a4
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-12-01 11:40:57 -0500

    likes

--- breddit/models.py
+++ breddit/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
   updated_at = models.DateTimeField(auto_now=True)
   link = models.URLField(default='https://www.google.com/', blank=True)
   thumbnail = models.ImageField(upload_to='thumbnails', default='oven.png')
-  likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='post_liked', blank=True)
+  likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='post_likes', blank=True)
   
   def __str__(self):
     return self.title
@@ -23,7 +23,7 @@ class Comment(models.Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='op')
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-  likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comment_liked', blank=True)
+  likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comment_likes', blank=True)
   
   def __str__(self):
     return self.owner
