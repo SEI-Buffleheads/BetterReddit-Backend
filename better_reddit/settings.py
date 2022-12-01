46d8c5b3b11163a80f4815c1bae1b6c9fa93a556
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-12-01 18:02:01 -0500

    token

--- better_reddit/settings.py
+++ better_reddit/settings.py
@@ -167,7 +167,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
